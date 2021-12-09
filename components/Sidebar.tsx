bb1a5b79fdf0b1a146fafa01939961357fe735d9
aashish-cd <076bct002@ioepc.edu.np>
2021-12-09 18:55:33 +0545

    only dark mode

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -73,12 +73,9 @@ const Sidebar = () => {
       >
         Email me
       </button>
-      <button
-        onClick={changeTheme}
-        className='w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 '
-      >
+      <button className='w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 '>
         {/* //TODO remove bg black */}
-        Toggle Theme
+        <a href='tel:9844682742'>Call me</a>
       </button>
     </>
   );
