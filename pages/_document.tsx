316b5410111e25cd26611f641ccba3913b626224
aashish-cd <076bct002@ioepc.edu.np>
2021-12-09 19:13:28 +0545

    sitemap added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <title>
+            Aashish Bhatt || आशिष भट्ट || Web developer , React developer, Full
+            stack developer from nepal
+          </title>
+          <meta
+            name='title'
+            content='Aashish Bhatt || आशिष भट्ट || Web developer , React developer, Full stack developer from nepal'
+          />
+          <meta
+            name='description'
+            content='Aashish Bhatt || आशिष भट्ट || Web developer , React developer, Full stack developer from nepal, TU IOE purwanchal campus dharan nepal, computer engineering student'
+          />
+          <meta
+            name='keywords'
+            content='react developer, web developer, front end developer, ioe dharan, ioe nepal, ioe tu, aashish bhatt , aashish bhatta, आशिष भट्ट '
+          />
+          <meta name='robots' content='index, follow' />
+          <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+          <meta name='language' content='English' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap'
+            rel='stylesheet'
           />
         </Head>
-        <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
+        <body className='bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white'>
           <Main />
           <NextScript />
         </body>
